b958e89459df4421ca281b1fdd08023720a01803
Peter Lavrov <dotaplay4fun@gmail.com>
2025-04-05 17:24:50 +0300

    adding vercel analytics

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <h2>Choose your destiny:</h2>
-    <ul>
+  <div class="home">
+    <h1>Choose your destiny:</h1>
+    <ul class="fighter-list">
       <li v-for="fighter in fighters" :key="fighter.name">
         <router-link :to="`/fighter/${fighter.name}`">
           {{ fighter.name }}
@@ -12,24 +12,40 @@
 </template>
 
 <script>
-import fightersData from '../data/fighters.json'; // Импортируем JSON
+import fightersData from '../data/fighters.json';
 
 export default {
   name: 'HomeView',
   data() {
     return {
-      fighters: fightersData // Присваиваем данные в состояние компонента
+      fighters: fightersData
     };
   }
 };
 </script>
 
-<style scoped>
-ul {
+<style lang="scss" scoped>
+.home {
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+.fighter-list {
   list-style: none;
   padding: 0;
-}
-li {
-  margin: 10px 0;
+
+  li {
+    margin: 10px 0;
+
+    a {
+      color: $link-blue;
+      text-decoration: none;
+      font-size: 1.2rem;
+
+      &:hover {
+        color: lighten($link-blue, 20%);
+      }
+    }
+  }
 }
 </style>
